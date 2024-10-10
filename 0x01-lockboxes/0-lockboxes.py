7a2d03928f30a573840326a47d931f8e32c69428
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-10 12:21:54 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,15 +8,15 @@ def canUnlockAll(boxes):
     unlocked_boxes = {0}
     queue = [0]
 
-    if not boxes:
-        return False
-
     while (queue):
         box = queue.pop(0)
-        if box > len(boxes) - 1:
+        if box > len(boxes):
             continue
         while (boxes[box]):
             element = boxes[box].pop(0)
             unlocked_boxes.add(element)
             queue.append(element)
-    return {x for x in range(len(boxes))} == unlocked_boxes
+
+    if (len(boxes) == len(unlocked_boxes)):
+        return True
+    return False
