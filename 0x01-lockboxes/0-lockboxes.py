666a214ee3b802bbbee08d0447ea2ea93ebb5e2f
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-10 11:47:17 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """ Lockboxes """
+    unlocked_boxes = {0}
+    queue = [0]
+    while (queue):
+        box = queue.pop(0)
+        while (boxes[box]):
+            element = boxes[box].pop(0)
+            unlocked_boxes.add(element)
+            queue.append(element)
+    if (len(unlocked_boxes) == len(boxes)):
+        return True
+    return False
