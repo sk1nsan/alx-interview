271964e287d86e68c788cfcf4838eb219ae7f32c
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-17 23:32:52 +0300

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,25 +2,18 @@
 """ Minimum Operations """
 
 
-def is_prime(n):
-    """ check if a number is a prime """
-    for i in range(2, int(n ** 0.5) + 1):
-        if (not n % i):
-            return False
-    return True
-
-
 def prime_decomp(n):
     """ returns the prime decomposition of n """
     primes = {}
     while (n != 1):
         for i in range(2, n + 1):
-            if (is_prime(i) and not n % i):
+            if (not n % i):
                 if i not in primes.keys():
                     primes[i] = 1
                 else:
                     primes[i] += 1
                 n = int(n / i)
+                break
     return primes
 
 
