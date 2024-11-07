5d2851c95aa4e85d29692335d88512918286110b
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-08 00:30:07 +0200

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -30,15 +30,15 @@ def solve_nqueens(n, row, board):
 if __name__ == '__main__':
     if (len(sys.argv) != 2):
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
     if (n < 4):
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
     solutions = []
     solve_nqueens(n, 0, [-1] * n)
     for solution in solutions:
