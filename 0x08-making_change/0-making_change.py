dea53d3273b4e32703183937e3917154ac58cc0c
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-11-28 23:52:52 +0200

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """return fewest number of coins needed to meet total"""
+    result = 0
+    if total <= 0:
+        return result
+    n = len(coins)
+    coins.sort()
+    for i in range(n):
+        j = coins[n - i - 1]
+        if total >= j:
+            result += total // j
+            total %= j
+        else:
+            break
+    if total != 0:
+        return -1
+    return result
