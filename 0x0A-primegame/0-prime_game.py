6d788f27b5c9404de84819e411e5608360e73c7f
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-12-11 22:38:39 +0200

    task 0

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""Prime Game"""
+
+
+def isWinner(x, nums):
+    """returns the winner of the game"""
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+    mariaWinsCount = 0
+    benWinsCount = 0
+
+    for num in nums:
+        roundsSet = list(range(1, num + 1))
+        primes = [n for n in range(1, num + 1) if is_prime(n)]
+        if not primes:
+            benWinsCount += 1
+            continue
+
+        isMariaTurns = True
+
+        while True:
+            if not primes:
+                if isMariaTurns:
+                    benWinsCount += 1
+                else:
+                    mariaWinsCount += 1
+                break
+
+            smallestPrime = primes.pop(0)
+            roundsSet.remove(smallestPrime)
+
+            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
+
+        isMariaTurns = not isMariaTurns
+
+    if mariaWinsCount > benWinsCount:
+        return "Maria"
+
+    if mariaWinsCount < benWinsCount:
+        return "Ben"
+
+    return None
+
+
+def is_prime(n):
+    """return True if prime False otherwise"""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
